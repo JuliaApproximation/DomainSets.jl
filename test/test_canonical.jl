b4ed27dce3faf9d0ca835755831f6b0c08f00a4a
Daan Huybrechs <daan.huybrechs@kuleuven.be>
2022-08-20 09:20:28 +0200

    improve support for infinite intervals

--- test/test_canonical.jl
+++ test/test_canonical.jl
@@ -3,4 +3,29 @@
     @test canonicaldomain(2..3) == ChebyshevInterval{Float64}()
     @test mapto(UnitInterval(), UnitInterval()) isa IdentityMap
     @test mapto(UnitInterval(), ChebyshevInterval()) isa AffineMap
+
+    @test canonicaldomain(2.0..Inf) isa ChebyshevInterval  # for now
+    @test canonicaldomain(OpenInterval(2.0,Inf)) === HalfLine{Float64,:open}()
+    @test canonicaldomain(OpenInterval(-Inf,2.0)) === HalfLine{Float64,:open}()
+    @test canonicaldomain(OpenInterval(-Inf,Inf)) === RealLine{Float64}()
+    @test canonicaldomain(OpenInterval(Inf,-Inf)) === RealLine{Float64}()
+    @test canonicaldomain(Interval{:closed,:open}(2.0,Inf)) === HalfLine{Float64,:closed}()
+    @test canonicaldomain(Interval{:closed,:open}(2.0,4.0)) == Interval{:closed,:open}(-1.0,1.0)
+    @test canonicaldomain(Interval{:open,:closed}(-Inf,2.0)) === HalfLine{Float64,:closed}()
+    @test canonicaldomain(Interval{:open,:closed}(1.0,2.0)) ==  Interval{:open,:closed}(-1.0,1.0)
+    @test_throws ArgumentError canonicaldomain(Interval{:closed,:open}(-Inf,Inf))
+    @test_throws ArgumentError canonicaldomain(Interval{:closed,:open}(-Inf,2.0))
+    @test_throws ArgumentError canonicaldomain(Interval{:open,:closed}(2.0,Inf))
+
+    @test mapfrom_canonical(OpenInterval(2.0,Inf))(0.0) == 2.0
+    @test mapfrom_canonical(OpenInterval(-Inf,2.0))(0.0) == 2.0
+    @test DomainSets.isidentity(mapfrom_canonical(OpenInterval(-Inf,Inf)))
+    @test mapfrom_canonical(OpenInterval(Inf,-Inf))(2.0) == -2.0
+    @test mapfrom_canonical(Interval{:closed,:open}(2.0,Inf))(0.0) == 2.0
+    @test mapfrom_canonical(Interval{:closed,:open}(2.0,4.0))(-1.0) == 2.0
+    @test mapfrom_canonical(Interval{:open,:closed}(-Inf,2.0))(0.0) == 2.0
+    @test mapfrom_canonical(Interval{:open,:closed}(1.0,2.0))(1.0) == 2.0
+    @test_throws ArgumentError mapfrom_canonical(Interval{:closed,:open}(-Inf,Inf))
+    @test_throws ArgumentError mapfrom_canonical(Interval{:closed,:open}(-Inf,2.0))
+    @test_throws ArgumentError mapfrom_canonical(Interval{:open,:closed}(2.0,Inf))
 end
