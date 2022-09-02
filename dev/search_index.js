250e9720593c246c4821ef78f9b0f4135909ae00
Documenter.jl <documenter@juliadocs.github.io>
2022-09-02 12:17:02 +0000

    build based on d19c627

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DomainSets.jl-Documentation","page":"Home","title":"DomainSets.jl Documentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This is only a temporary document for now.","category":"page"}]
+}
