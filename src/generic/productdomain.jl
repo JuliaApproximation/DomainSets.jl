540b5b3b3fc2178e2dac1c71ebeca9605a728057
philip-stuckey <developer.philip_stuckey@fastmail.com>
2023-06-27 12:26:16 -0400

    changed `hcat` to `vcat`

--- src/generic/productdomain.jl
+++ src/generic/productdomain.jl
@@ -51,7 +51,7 @@ rightendpoint(d::ProductDomain) = toexternalpoint(d, map(rightendpoint, componen
 interior(d::ProductDomain) = ProductDomain(map(interior, components(d)))
 closure(d::ProductDomain) = ProductDomain(map(closure, components(d)))
 
-center(d::ProductDomain) = hcat(map(center, factors(d))...)
+center(d::ProductDomain) = vcat(map(center, factors(d))...)
 
 VcatDomainElement = Union{Domain{<:Number},EuclideanDomain}
 
